e919edb358198de857574e55962e7dab8b0132b7
Phillip Ou <phillipou94@gmail.com>
2020-07-12 17:35:32 -0700

    created scrolling behavior

--- javascript/app.js
+++ javascript/app.js
@@ -10,36 +10,11 @@ $(document).ready(function(){
         }, 2000);
     });
 
-   $('.scroll-to-skills').click(function(){
-        var nextSection = $('#skills');
+   $('.scroll-to-experience-and-projects').click(function(){
+        var nextSection = $('#experience-header');
         $('html, body').animate({
-            scrollTop: $(nextSection).offset().top
+            scrollTop: $(nextSection).offset().top - 50
         }, 2000);
     });
-
-   $('.scroll-to-portfolio').click(function(){
-        var nextSection = $('#portfolio');
-        $('html, body').animate({
-            scrollTop: $(nextSection).offset().top
-        }, 2000);
-    });
-
-   $('.scroll-to-experience').click(function(){
-        var nextSection = $('#experience');
-        $('html, body').animate({
-            scrollTop: $(nextSection).offset().top
-        }, 2000);
-    });
-
-    $(".project").hover(function(){
-
-        $(this).css("border", "8px solid #ff3d2b");
-        $(this).css("border-width", "0 0 0 2px");
-       
-        },function(){
-         $(this).find(".project-logo").css("border","2px solid rgba(245,245,245,1)");
-         $(this).css("border-style", "none");
-    });
-
 });
 
